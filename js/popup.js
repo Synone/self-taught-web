7373bca1164f57cfd9f3e54cbc010493afe20563
Sony <syno3311@gmail.com>
2022-03-01 16:09:59 +0700

    first-commit

---
+++ js/popup.js
@@ -0,0 +1,24 @@
+const enrollBtns = document.querySelectorAll('.enroll-btn');
+const popupContent = document.querySelector('.popup__content');
+var popup = document.querySelector('.popup');
+var popupClose = document.querySelector('.popup__close');
+
+function addClass() {
+    popup.classList.add('open')
+}
+
+for(const enrollBtn of enrollBtns){
+    enrollBtn.addEventListener('click',addClass)
+}
+
+popupClose.onclick = function () {
+    popup.classList.remove('open')
+}
+popup.onclick = function () {
+    popup.classList.remove('open')
+}
+
+popupContent.addEventListener('click',function (event){  
+    event.stopImmediatePropagation()
+    
+})
